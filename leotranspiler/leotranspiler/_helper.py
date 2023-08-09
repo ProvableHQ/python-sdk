327e0fb86952b423a3acbc1d9ea5c12707716cad
Konstantin Pandl <konstantinpandl@yahoo.de>
2023-08-09 12:48:11 +0200

    fix decimal places function for integers

--- leotranspiler/leotranspiler/_helper.py
+++ leotranspiler/leotranspiler/_helper.py
@@ -1,4 +1,6 @@
 def _get_rounding_decimal_places(value, max_places=10):
+    if value == int(value):
+        return 0
     for i in range(1, max_places + 1):
         rounded_val = round(value, i)
         if abs(value - rounded_val) < 10**(-i - 1): 
