f648d068d57a68d705f5a7983c2a94a18c90eb1e
Konstantin Pandl <konstantinpandl@yahoo.de>
2023-08-24 17:07:08 +0200

    continue work on integer representation

--- leotranspiler/leotranspiler/_leo_helper.py
+++ leotranspiler/leotranspiler/_leo_helper.py
@@ -1,8 +1,23 @@
 # -*- coding: utf-8 -*-
+from typing import Optional, Union
+
 _leo_type_bits = [2**3, 2**4, 2**5, 2**6, 2**7]
 
 
-def _get_leo_integer_type(signed, value_bits):
+def _get_leo_integer_bits(signed: bool, value_bits: int):
+    for bits in _leo_type_bits:
+        # Subtract 1 bit for signed integers
+        max_bits = bits - 1 if signed else bits
+        if value_bits <= max_bits:
+            return bits
+
+    raise ValueError(
+        f"No leo type for {'signed' if signed else 'unsigned'} value with more than "
+        f"{_leo_type_bits[-1]} bits. Try quantizing the model and/or the data."
+    )
+
+
+def _get_leo_integer_type(signed: bool, value_bits: int):
     for bits in _leo_type_bits:
         # Subtract 1 bit for signed integers
         max_bits = bits - 1 if signed else bits
@@ -15,10 +30,16 @@ def _get_leo_integer_type(signed, value_bits):
     )
 
 
-class LeoType:
-    """Leo type."""
+class LeoInteger:
+    """Leo Integer."""
 
-    def __init__(self, value, type):
+    def __init__(
+        self,
+        signed: bool,
+        bits: int,
+        original_value: Union[int, float],
+        fixed_point_scaling_factor: Optional[int] = None,
+    ):
         """Leo type.
 
         Parameters
@@ -33,5 +54,12 @@ class LeoType:
         LeoType
             The Leo type.
         """
-        self.value = value
+        self.original_value = original_value
         self.type = type
+
+        self.fixed_point_value = None  # todo
+        self.decimal_value = None  # todo
+
+        self.leo_value = self._get_leo_value(
+            self.fixed_point_value, signed, bits, fixed_point_scaling_factor
+        )
