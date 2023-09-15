fix integer overflow bug

leotranspiler/leotranspiler/_model_transpiler.py
@@ -99,7 +99,10 @@ class _ModelTranspilerBase:
 
     def _convert_to_fixed_point(self, value):
         if hasattr(value, "shape"):  # check if value is a numpy array
-            return (value * self.fixed_point_scaling_factor).astype(int)
+            vectorized_int = np.vectorize(int)
+            return vectorized_int(
+                value.astype(object) * self.fixed_point_scaling_factor
+            )
         else:
             return int(round(value * self.fixed_point_scaling_factor))
 

leotranspiler/tests/test_leo_transpiler.py
@@ -400,13 +400,16 @@ class TestLeoTranspiler(unittest.TestCase):
         # Compute the accuracy of the Leo program and the Python program on the test set
         num_test_samples = len(test_images_2d)
 
-        # let's limit the number of test stamples to 50 to make the computation faster
-        num_test_samples = min(num_test_samples, 50)
+        # let's limit the number of test stamples to 10 to make the computation faster
+        num_test_samples = min(num_test_samples, 10)
 
         python_predictions = clf.predict(test_images_2d)
 
         leo_predictions = np.zeros(num_test_samples)
         for i in range(num_test_samples):
+            one_dim_array = test_images_2d[i].ravel()  # noqa: F841
+            inputs = lt.model_transpiler.generate_input(test_images_2d[i])  # noqa: F841
+
             leo_predictions[i] = lt.run(input_sample=test_images_2d[i]).output_decimal[
                 0
             ]