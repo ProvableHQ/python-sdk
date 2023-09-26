5bb8461da0946a6d48eaaf4abf317018cdc8e8d0
Konstantin Pandl <konstantinpandl@yahoo.de>
2023-09-26 16:11:28 +0200

    expand binary image case, L1 regularization using PyTorch, SVM for haar features

--- zkml-research/MNIST_preprocessing/helper.py
+++ zkml-research/MNIST_preprocessing/helper.py
@@ -64,3 +64,50 @@ def prune_neural_network(
         )
     )
     return network_to_prune
+
+
+def prune_pytorch_network(
+    model, weight_threshold=1e-1, bias_threshold=1e-1
+):  # noqa: D103
+    num_weights = 0
+    num_changed_weights = 0
+    num_biases = 0
+    num_changed_biases = 0
+
+    # Pruning the weights
+    for name, param in model.named_parameters():
+        if "weight" in name:
+            flattened_weights = param.data.view(-1)
+            for j, weight in enumerate(flattened_weights):
+                if abs(weight) < weight_threshold:
+                    flattened_weights[j] = 0
+                    num_changed_weights += 1
+                num_weights += 1
+            param.data = flattened_weights.view(param.data.shape)
+        elif "bias" in name:
+            flattened_biases = param.data.view(-1)
+            for j, bias in enumerate(flattened_biases):
+                if abs(bias) < bias_threshold:
+                    flattened_biases[j] = 0
+                    num_changed_biases += 1
+                num_biases += 1
+            param.data = flattened_biases.view(param.data.shape)
+
+    print(f"Number of weight parameters: {num_weights}")  # noqa: T201
+    print(f"Number of changed weight parameters: {num_changed_weights}")  # noqa: T201
+    print(f"Number of bias parameters: {num_biases}")  # noqa: T201
+    print(f"Number of changed bias parameters: {num_changed_biases}")  # noqa: T201
+    print(  # noqa: T201
+        f"Percentage of weights pruned: {num_changed_weights / num_weights * 100:.2f}%"
+    )
+    print(  # noqa: T201
+        f"Percentage of biases pruned: {num_changed_biases / num_biases * 100:.2f}%"
+    )
+    print(  # noqa: T201
+        f"Remaining number of non-zero weights: {num_weights - num_changed_weights}"
+    )
+    print(  # noqa: T201
+        f"Remaining number of non-zero biases: {num_biases - num_changed_biases}"
+    )
+
+    return model
