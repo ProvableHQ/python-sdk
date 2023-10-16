specifiable fixed-point scaling factor, fixing tests

zkml/tests/test_leo_transpiler.py
@@ -814,7 +814,7 @@ class TestLeoTranspiler(unittest.TestCase):
         lt = LeoTranspiler(model=sklearn_mlp, validation_data=training_data)
         leo_project_path = os.path.join(os.getcwd(), library_name + "/tests/tmp/mnist")
         leo_project_name = "sklearn_mlp_mnist_1"
-        lt.to_leo(path=leo_project_path, project_name=leo_project_name)
+        lt.to_leo(path=leo_project_path, project_name=leo_project_name, fixed_point_scaling_factor=16)
 
         python_prediction = sklearn_mlp.predict([training_data[0]])
         lc = lt.run(training_data[0])
@@ -880,7 +880,7 @@ class TestLeoTranspiler(unittest.TestCase):
         lt = LeoTranspiler(model=sklearn_mlp, validation_data=training_data)
         leo_project_path = os.path.join(os.getcwd(), library_name + "/tests/tmp/mnist")
         leo_project_name = "sklearn_mlp_mnist_1"
-        lt.to_leo(path=leo_project_path, project_name=leo_project_name)
+        lt.to_leo(path=leo_project_path, project_name=leo_project_name, fixed_point_scaling_factor=16)
 
         python_prediction = sklearn_mlp.predict(training_data)
         lc = lt.run(training_data)
@@ -941,8 +941,8 @@ class TestLeoTranspiler(unittest.TestCase):
 
         clf = prepare_MNIST_MLP(train_features_resized_normalized, val_features_resized_normalized, test_features_resized_normalized, train_labels_tensor, validation_labels_tensor, test_labels)
 
-        train_images_2d = train_images_2d.numpy()
-        test_images_2d = test_images_2d.numpy()
+        train_images_2d = train_features_resized_normalized.numpy()
+        test_images_2d = test_features_resized_normalized.numpy()
 
         import logging
         import os
@@ -959,7 +959,7 @@ class TestLeoTranspiler(unittest.TestCase):
         lt = LeoTranspiler(model=clf, validation_data=train_images_2d[0:200])
         leo_project_path = os.path.join(os.getcwd(), library_name + "/tests/tmp/mnist")
         leo_project_name = "sklearn_mlp_mnist_1"
-        lt.to_leo(path=leo_project_path, project_name=leo_project_name)
+        lt.to_leo(path=leo_project_path, project_name=leo_project_name, fixed_point_scaling_factor=16)
 
         # Compute the accuracy of the Leo program and the Python program on the test set
         num_test_samples = len(test_images_2d)
@@ -971,12 +971,13 @@ class TestLeoTranspiler(unittest.TestCase):
 
         leo_predictions = np.zeros(num_test_samples)
         for i in range(num_test_samples):
-            one_dim_array = test_images_2d[i].ravel()  # noqa: F841
-            inputs = lt.model_transpiler.generate_input(test_images_2d[i])  # noqa: F841
+            # one_dim_array = test_images_2d[i].ravel()  # noqa: F841
+            # inputs = lt.model_transpiler.generate_input(test_images_2d[i])  # noqa: F841
 
             lc = lt.run(input=test_images_2d[i])
+            leo_predictions[i] = np.argmax(lc.output_decimal)
 
-            self.assertEqual(int(leo_predictions[i]), python_predictions[i])
+        accuracy_score = np.sum(leo_predictions == python_predictions[0:num_test_samples]) / num_test_samples
 
 
     def test_mlp_mnist_benchmark(self):  # noqa: D103
@@ -1029,7 +1030,7 @@ class TestLeoTranspiler(unittest.TestCase):
             lt = LeoTranspiler(model=clf, validation_data=train_images_2d[0:200])
             leo_project_path = os.path.join(os.getcwd(), library_name + "/tests/tmp/mnist")
             leo_project_name = "sklearn_mlp_mnist_1"
-            lt.to_leo(path=leo_project_path, project_name=leo_project_name)
+            lt.to_leo(path=leo_project_path, project_name=leo_project_name, fixed_point_scaling_factor=16)
 
             # Compute the accuracy of the Leo program and the Python program on the test set
             num_test_samples = len(test_images_2d)