3eaafbff14693aaf79759c290fe87edd1b3a8364
Konstantin Pandl <konstantinpandl@yahoo.de>
2023-12-29 00:17:35 +0100

    transpilation

--- zkml-research/zkmlaas/app.py
+++ zkml-research/zkmlaas/app.py
@@ -1,6 +1,24 @@
 from flask import Flask, send_from_directory, request, jsonify
+from zkml import LeoTranspiler
+import sklearn
 
 app = Flask(__name__, static_folder='frontend', static_url_path='/static')
+# ensure tmp folder in current directory exists
+import os
+tmp_path = os.path.join(os.getcwd(), "zkml-research", "zkmlaas", "tmp")
+os.makedirs(tmp_path, exist_ok=True)
+
+# remove all files in tmp folder
+import shutil
+for filename in os.listdir(tmp_path):
+    file_path = os.path.join(os.getcwd(), "tmp", filename)
+    try:
+        if os.path.isfile(file_path) or os.path.islink(file_path):
+            os.unlink(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
+    except Exception as e:
+        print(f"Failed to delete {file_path}. Reason: {e}")
 
 # Global lists for storing model information
 model_names = []
@@ -9,8 +27,19 @@ model_data = []
 # Dummy functions
 def transpile(file_content):
     # Insert the transpilation logic here
+    # load sklearn model. It is stored as clf in a pickle file
+    import pickle
+    model = pickle.loads(file_content)
+
+
+    
+    
+    lt = LeoTranspiler(model=model)
+    model_name = f"model_{len(model_names)}, {model.__class__.__name__}"
+    leo_project_name = f"project_{len(model_names)}"
+    lt.to_leo(path=tmp_path, project_name=leo_project_name)
     # Example: return "model_name", {"num_inputs": 3}
-    return "model_name", {"num_inputs": 3}
+    return model_name, {"num_inputs": 3}
 
 def proof(*args):
     # Insert the calculation logic here
