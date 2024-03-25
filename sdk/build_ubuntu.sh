4e1acbc664194b6ea354ba67915ef728dd6a01b3
Konstantin Pandl <konstantinpandl@yahoo.de>
2024-03-25 16:48:58 +0100

    add ubuntu build script

---
+++ sdk/build_ubuntu.sh
@@ -0,0 +1,27 @@
+# Install Ubuntu dependencies
+
+sudo apt-get update
+sudo apt-get install -y \
+	build-essential \
+	curl \
+	clang \
+	gcc \
+	libssl-dev \
+	llvm \
+	make \
+	pkg-config \
+	tmux \
+	xz-utils \
+	ufw
+
+
+# Install Rust
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+# Install python sdk
+python3 -m venv .env
+source .env/bin/activate
+pip install maturin
+maturin build
